c1140a3c996fa2df5bc0c59cb341e121788d9a45
Blackmesa-Canteen <blackmesa_canteen@riseup.net>
2021-07-05 23:35:54 +0800

    add Goods info activity

--- app/src/main/java/com/example/marketproject/app/GoodsInfoActivity.java
+++ app/src/main/java/com/example/marketproject/app/GoodsInfoActivity.java
@@ -3,14 +3,115 @@ package com.example.marketproject.app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.marketproject.R;
 
-public class GoodsInfoActivity extends AppCompatActivity {
+public class GoodsInfoActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageButton ibGoodInfoBack;
+    private ImageButton ibGoodInfoMore;
+    private ImageView ivGoodInfoImage;
+    private TextView tvGoodInfoName;
+    private TextView tvGoodInfoDesc;
+    private TextView tvGoodInfoPrice;
+    private TextView tvGoodInfoStore;
+    private TextView tvGoodInfoStyle;
+    private WebView wbGoodInfoMore;
+    private LinearLayout llGoodsRoot;
+    private TextView tvGoodInfoCallcenter;
+    private TextView tvGoodInfoCollection;
+    private TextView tvGoodInfoCart;
+    private Button btnGoodInfoAddcart;
+    private TextView tv_more_share;
+    private TextView tv_more_search;
+    private TextView tv_more_home;
+    private Button btn_more;
+
+
+    /**
+     * Find the Views in the layout<br />
+     * <br />
+     * Auto-created on 2021-07-05 22:26:12 by Android Layout Finder
+     * (http://www.buzzingandroid.com/tools/android-layout-finder)
+     */
+    private void findViews() {
+        ibGoodInfoBack = (ImageButton)findViewById( R.id.ib_good_info_back );
+        ibGoodInfoMore = (ImageButton)findViewById( R.id.ib_good_info_more );
+        ivGoodInfoImage = (ImageView)findViewById( R.id.iv_good_info_image );
+        tvGoodInfoName = (TextView)findViewById( R.id.tv_good_info_name );
+        tvGoodInfoDesc = (TextView)findViewById( R.id.tv_good_info_desc );
+        tvGoodInfoPrice = (TextView)findViewById( R.id.tv_good_info_price );
+        tvGoodInfoStore = (TextView)findViewById( R.id.tv_good_info_store );
+        tvGoodInfoStyle = (TextView)findViewById( R.id.tv_good_info_style );
+        wbGoodInfoMore = (WebView)findViewById( R.id.wb_good_info_more );
+        llGoodsRoot = (LinearLayout)findViewById( R.id.ll_goods_root );
+
+        tvGoodInfoCallcenter = (TextView)findViewById( R.id.tv_good_info_callcenter );
+        tvGoodInfoCollection = (TextView)findViewById( R.id.tv_good_info_collection );
+        tvGoodInfoCart = (TextView)findViewById( R.id.tv_good_info_cart );
+        btnGoodInfoAddcart = (Button)findViewById( R.id.btn_good_info_addcart );
+
+        ibGoodInfoBack.setOnClickListener( this );
+        ibGoodInfoMore.setOnClickListener( this );
+        btnGoodInfoAddcart.setOnClickListener( this );
+
+        tvGoodInfoCallcenter.setOnClickListener(this);
+        tvGoodInfoCollection.setOnClickListener(this);
+        tvGoodInfoCart.setOnClickListener(this);
+
+        tv_more_share = (TextView) findViewById(R.id.tv_more_share);
+        tv_more_search = (TextView) findViewById(R.id.tv_more_search);;
+        tv_more_home = (TextView) findViewById(R.id.tv_more_home);
+        btn_more = (Button) findViewById(R.id.btn_more);
+    }
+
+    /**
+     * Handle button click events<br />
+     * <br />
+     * Auto-created on 2021-07-05 22:26:12 by Android Layout Finder
+     * (http://www.buzzingandroid.com/tools/android-layout-finder)
+     */
+    @Override
+    public void onClick(View v) {
+        if ( v == ibGoodInfoBack ) {
+            // Handle clicks for ibGoodInfoBack
+            finish();
+        } else if ( v == ibGoodInfoMore ) {
+            // Handle clicks for ibGoodInfoMore
+            Toast.makeText(this, "more info", Toast.LENGTH_SHORT).show();
+        } else if ( v == btnGoodInfoAddcart ) {
+            // Handle clicks for btnGoodInfoAddcart
+            Toast.makeText(this, "Add to cart", Toast.LENGTH_SHORT).show();
+        } else if (v == tvGoodInfoCallcenter) {
+            Toast.makeText(this, "客服", Toast.LENGTH_SHORT).show();
+        } else if ( v == tvGoodInfoCollection) {
+            Toast.makeText(this, "收藏", Toast.LENGTH_SHORT).show();
+        } else if ( v == tvGoodInfoCart) {
+            Toast.makeText(this, "购物车", Toast.LENGTH_SHORT).show();
+        } else if ( v == tv_more_share) {
+            Toast.makeText(this, "more share", Toast.LENGTH_SHORT).show();
+        } else if (v == tv_more_search) {
+            Toast.makeText(this, "more search", Toast.LENGTH_SHORT).show();
+        } else if (v == tv_more_home) {
+            Toast.makeText(this, "more home", Toast.LENGTH_SHORT).show();
+        } else if (v == btn_more) {
+            Toast.makeText(this, "btn more", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goods_info);
+        findViews();
     }
 }
